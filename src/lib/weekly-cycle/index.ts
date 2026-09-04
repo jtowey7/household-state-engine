36e297dec8ea8b0946df4f85058badd6231b587c
jtowey7 <30824437+jtowey7@users.noreply.github.com>
2026-09-04 20:58:29 +0100

    feat: export weekly learning composition

--- src/lib/weekly-cycle/index.ts
+++ src/lib/weekly-cycle/index.ts
@@ -2,4 +2,5 @@ export * from "./types";
 export * from "./cycle";
 export { runJudgedWeeklyShadowCycle } from "./judged";
 export { runWeeklyShadowCycleWithConsumptionReconciliation } from "./consumption-reconciliation";
+export { runWeeklyShadowCycleWithLearning } from "./learning";
 export { weeklyScope, weeklyPort, weeklyPlan, weeklyAsOf, weeklyNow } from "./fixtures";
